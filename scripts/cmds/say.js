2e51a89e59a45835086d520b4000024be86aa0f6
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-02-07 16:14:02 -0800

    Update say.js

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -1,40 +1,43 @@
-const axios = require("axios");
-
-const baseApiUrl = async () => {
- const base = await axios.get("https://raw.githubusercontent.com/mahmudx7/exe/main/baseApiUrl.json");
- return base.data.mahmud
+module.exports.config = {
+ 'name': 'say',
+ 'version': "1.0.1",
+ 'hasPermssion': 0x0,
+ 'credits': "ALIHSAN SHOUROV",
+ 'description': "Make the bot return google's audio file via text",
+ 'commandCategory': "media",
+ 'usages': "[bn] [Text]",
+ 'cooldowns': 0x5,
+ 'dependencies': {
+ 'path': '',
+ 'fs-extra': ''
+ }
 };
-
-module.exports = {
- config: {
- name: "say",
- version: "1.7",
- author: "Chitron Bhattacharjee",
- countDown: 5,
- role: 0,
- category: "media",
- guide: "{pn} <text> (or reply to a message)",
- },
-
- onStart: async function ({ api, message, args, event }) {
- let text = args.join(" ");
-
- if (event.type === "message_reply" && event.messageReply.body) {
- text = event.messageReply.body;
- }
-
- if (!text) {
- return message.reply("⚠️ দয়া করে কিছু লিখুন বা একটি মেসেজে রিপ্লাই দিন!");
- }
-
+module.exports.run = async function ({
+ api: _0x5cee6f,
+ event: _0x6b558b,
+ args: _0x5c8dc1
+}) {
  try {
- const baseUrl = await baseApiUrl();
- const response = await axios.get(`${baseUrl}/api/say`, {
- params: { text },
- headers: { "Author": module.exports.config.author },
- responseType: "stream",
- });
-
+ const {
+ createReadStream: _0x19316d,
+ unlinkSync: _0x1b899a
+ } = global.nodemodule["fs-extra"];
+ const {
+ resolve: _0x1b6f6e
+ } = global.nodemodule.path;
+ var _0x50a304 = _0x6b558b.type == "message_reply" ? _0x6b558b.messageReply.body : _0x5c8dc1.join(" ");
+ var _0x173460 = ['bn'].some(_0x657a78 => _0x50a304.indexOf(_0x657a78) == 0) ? _0x50a304.slice(0, _0x50a304.indexOf(" ")) : global.config.language;
+ var _0xa243c2 = _0x173460 != global.config.language ? _0x50a304.slice(3, _0x50a304.length) : _0x50a304;
+ const _0x5e6854 = _0x1b6f6e(__dirname, "cache", _0x6b558b.threadID + '_' + _0x6b558b.senderID + ".mp3");
+ await global.utils.downloadFile("https://translate.google.com/translate_tts?ie=UTF-8&q=" + encodeURIComponent(_0xa243c2) + "&tl=bn&client=tw-ob", _0x5e6854);
+ return _0x5cee6f.sendMessage({
+ 'attachment': _0x19316d(_0x5e6854)
+ }, _0x6b558b.threadID, () => _0x1b899a(_0x5e6854));
+ } catch (_0x1008a6) {
+ return console.log(_0x1008a6);
+ }
+ ;
+};
  if (response.data.error) {
  return message.reply(`❌ Error: ${response.data.error}`);
  }
