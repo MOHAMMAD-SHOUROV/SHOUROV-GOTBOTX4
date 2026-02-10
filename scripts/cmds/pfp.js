5026e49df11146b9505169529607e8cb2ad9f961
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-02-10 01:01:58 -0800

    Update pfp.js

--- scripts/cmds/pfp.js
+++ scripts/cmds/pfp.js
@@ -1,108 +1,54 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
+const { GoatWrapper } = require("fca-liane-utils");
 
 module.exports = {
   config: {
-    name: "pfp",
-    aliases: ["avatar", "profilepic"],
-    version: "1.1",
-    author: "alihsan shourov (fixed)",
+    name: "profile",
+    aliases: ["pp", "pfp"],
+    version: "1.2",
+    author: "NIB (fixed by Mahbub Ullash)",
     countDown: 5,
     role: 0,
-    description: {
-      vi: "Lấy ảnh đại diện của người dùng",
-      en: "Fetch user's profile picture"
-    },
-    category: "utility",
+    shortDescription: "Facebook profile picture",
+    longDescription: "See anyone's Facebook profile picture (self/reply/mention/link).",
+    category: "image",
     guide: {
-      en:
-        "{pn}\n" +
-        "{pn} @tag\n" +
-        "{pn} uid\n" +
-        "{pn} facebook profile link\n" +
-        "(or reply to someone)"
+      en: "{pn} [reply/@mention/link]"
     }
   },
 
-  langs: {
-    en: {
-      fetching: "🔍 Fetching profile picture...",
-      success: "✓ Profile picture of %1",
-      error: "× Could not fetch profile picture: %1",
-      invalidUID: "! Invalid UID"
-    }
-  },
-
-  onStart: async function ({ api, message, args, event, getLang, usersData }) {
+  onStart: async function ({ api, event, args, message, usersData }) {
     try {
-      let uid = event.senderID;
+      let uid;
 
-      /* ===== UID RESOLVE ===== */
-      if (event.messageReply?.senderID) {
+      if (event.type === "message_reply" && event.messageReply?.senderID) {
         uid = event.messageReply.senderID;
       }
+
       else if (event.mentions && Object.keys(event.mentions).length > 0) {
         uid = Object.keys(event.mentions)[0];
       }
-      else if (args[0]) {
-        if (!isNaN(args[0])) {
-          uid = args[0];
-        }
-        else if (args[0].includes("facebook.com")) {
-          // profile.php?id=
-          let match = args[0].match(/profile\.php\?id=(\d+)/);
-          if (match) {
-            uid = match[1];
-          } else {
-            // vanity username
-            const vanity = args[0].match(/facebook\.com\/([^/?]+)/);
-            if (vanity) {
-              try {
-                const res = await axios.get(`https://www.facebook.com/${vanity[1]}`);
-                const uidMatch = res.data.match(/"userID":"(\d+)"/);
-                if (uidMatch) uid = uidMatch[1];
-              } catch {
-                return message.reply(getLang("error", "Cannot extract UID from link"));
-              }
-            }
-          }
-        }
+
+      else if (args[0] && args[0].includes(".com/")) {
+        uid = await api.getUID(args[0]);
       }
 
-      if (!uid || isNaN(uid)) {
-        return message.reply(getLang("invalidUID"));
+      else {
+        uid = event.senderID;
       }
 
-      await message.reply(getLang("fetching"));
+      const name = await usersData.getName(uid);
 
-      /* ===== USER NAME ===== */
-      const userName =
-        ((await usersData.get(uid))?.name ||
-          (await api.getUserInfo(uid))?.[uid]?.name ||
-          "Unknown User");
+      const imageUrl = `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
-      /* ===== GRAPH URL (UNCHANGED) ===== */
-      const avatarURL =
-        `https://graph.facebook.com/${uid}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-
-      const cacheDir = path.join(__dirname, "cache");
-      const cachePath = path.join(cacheDir, `pfp_${uid}.jpg`);
-      await fs.ensureDir(cacheDir);
-
-      const res = await axios.get(avatarURL, { responseType: "arraybuffer" });
-      fs.writeFileSync(cachePath, res.data);
-
-      await message.reply({
-        body: getLang("success", userName),
-        attachment: fs.createReadStream(cachePath)
+      return message.reply({
+        body: ` `,
+        attachment: await global.utils.getStreamFromURL(imageUrl)
       });
-
-      fs.unlinkSync(cachePath);
-
-    } catch (err) {
-      console.error("PFP ERROR:", err);
-      return message.reply(getLang("error", err.message));
+    } catch (e) {
+      return message.reply("⚠️");
     }
   }
 };
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
